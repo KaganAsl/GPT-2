bd519205515c181e79ad2af8b3ae69f99d8e7ef0
Kagan <kagan.aslancan@gmail.com>
2026-05-24 16:25:20 +0300

    gpt2 layer

--- models/gpt2.py
+++ models/gpt2.py
@@ -1,4 +1,5 @@
 import torch
+import torch
 from torch import nn
 from transformers import GPT2Model as OpenAIGPT2Model
 
@@ -48,19 +49,13 @@ class GPT2Model(GPTPreTrainedModel):
     seq_length = input_shape[1]
 
     inputs_embeds = None
-
-    ### YOUR CODE HERE
-    raise NotImplementedError
-
+    inputs_embeds = self.word_embedding(input_ids)
 
     pos_ids = self.position_ids[:, :seq_length]
     pos_embeds = None
+    pos_embeds = self.pos_embedding(pos_ids)
 
-    ### TODO: Use pos_ids to get position embedding from self.pos_embedding into pos_embeds.
-    ###       Then, add two embeddings together; then apply dropout and return.
-    ### YOUR CODE HERE
-    raise NotImplementedError
-
+    return self.embed_dropout(inputs_embeds + pos_embeds)
 
   def encode(self, hidden_states, attention_mask):
     """
@@ -105,8 +100,7 @@ class GPT2Model(GPTPreTrainedModel):
 
       return hidden_state(s) * E^T
     """
-    ### YOUR CODE HERE
-    raise NotImplementedError
+    return torch.matmul(hidden_state, self.word_embedding.weight.T)
 
 
   @classmethod
